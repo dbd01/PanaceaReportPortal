2646d0a45c2070e15058fae4b98b2ffe94fa0a76
d.charlaftis <d.charlaftis@dotbydot.gr>
2015-09-17 13:33:02 +0300

    fix

---
+++ app/layout/layoutController.js
@@ -0,0 +1,13 @@
+	"use strict";
+	
+  app.controller('layoutController', ['$scope', function($scope) {
+		$scope.lala=1;
+	}])
+	
+	.directive("sidebar-directive", function (){
+		return {
+		restrict: 'EA',
+		templateUrl: 'app/layout/views/sidebarView.html'			
+		};
+	});
+ 
--- app/layout/sidebarDirective.js
+++
@@ -1,9 +0,0 @@
-	"use strict";
-
-	app.directive("sidebar-directive", [sidebar-directive], function (){
-		return {
-		restrict: 'EA',
-		templateUrl: 'app/layout/views/sidebarView.html'			
-		};
-	});
- 
