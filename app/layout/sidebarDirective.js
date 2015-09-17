31479cf3b71273e898eba3d1617ee0f57795e65f
d.charlaftis <d.charlaftis@dotbydot.gr>
2015-09-17 12:46:35 +0300

    initializing views, directives, controllers

---
+++ app/layout/sidebarDirective.js
@@ -0,0 +1,11 @@
+(function () {
+    "use strict";
+
+    app.directive("sidebar-directive", [ sidebar-directive])
+
+    return {
+			restrict: 'EA',
+			templateUrl: 'app/layout/views/sidebarView.html'			
+		};
+		
+})();
