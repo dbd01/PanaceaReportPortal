085d0b2fdcc789e51ef03289d126e158b90cb295
d.charlaftis <d.charlaftis@dotbydot.gr>
2015-09-22 14:39:55 +0300

    headers

--- app/appConfig.js
+++ app/appConfig.js
@@ -4,6 +4,7 @@ app
     .config(['$httpProvider',function ($httpProvider) {
         //$httpProvider.interceptors.push('authInterceptorService');
         $httpProvider.defaults.useXDomain = true;
+        $httpProvider.defaults.headers.common = 'Content-Type: application/json';
         delete $httpProvider.defaults.headers.common['X-Requested-With'];
     }]);
     
