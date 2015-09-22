bcfb189b74aded2ecc41b7d4c7f0755c64aa0c14
d.charlaftis <d.charlaftis@dotbydot.gr>
2015-09-22 15:36:52 +0300

    authorization ok

--- app/auth/controllers/authenticationController.js
+++ app/auth/controllers/authenticationController.js
@@ -2,9 +2,7 @@
 
     app.controller('authenticationController', ['$scope', 'authenticationService','appSettings', function ($scope, authenticationService, appSettings) {
 			
-			$scope.username = "";
-			$scope.password = "";
-						
+									
 			//send auth data to auth server
 			$scope.authData = {
            		 username: $scope.username,
@@ -13,26 +11,23 @@
         		};
 
         	$scope.login = function () {
-            	authenticationService.send($scope.authData, function (response) {
-	                console.log("authentication data been sent successfully");
-	                console.log("auth data :",$scope.authData);
-	                console.log("response:",response );
-	                //$location.path('/portal/portals');
+        		authenticationService.send($scope.authData, 
+            		function (response) {
+		                console.log("authentication data been sent successfully");    
+		                console.log(response.token);          
+		                //$location.path('/portal/portals');
 	            	},
             	 	function (response) {
-	                	console.log("datta..",$scope.authData);
-		                if (response.data == null)	                
-		                   console.log("response data is null!");	               
-		                else
-		                {
-		                   var errors = [];
-		                   for (var key in response.data.modelState) {
-		                        for (var i = 0; i < response.data.modelState[key].length; i++) {
-		                           errors.push(response.data.modelState[key][i]);
-		                        }
-		                    }
-		                    console.log("Failed to add portal due to: " + errors.join(' '));
-		                }
+	                	console.log("data..>",$scope.authData);
+		                if (response.data == null)	{                
+		                   console.log("response data is null!");
+		                   $scope.message = "Server error";
+		                }               
+		                else{
+		                  console.log("response error: ", response);
+		               	  $scope.message = "Authorization error...";
+		                }                  
+		                
              		});            
         		};
 
