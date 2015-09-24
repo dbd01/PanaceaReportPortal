43013b9ede59a68b05fbf574452db38e6f8784ec
d.charlaftis <d.charlaftis@dotbydot.gr>
2015-09-24 17:52:27 +0300

    test

--- app/users/controllers/usersController.js
+++ app/users/controllers/usersController.js
@@ -6,36 +6,36 @@ app.controller("usersController", ['localStorageService','usersService','$scope'
 	var authData = localStorageService.get('authorizationData');
 	console.log("auth data--uu>", authData);	
     
-    var usersTable = {
-            "header": [
-            { "title": "id" },
-            { "title": "Domain" },
-            { "title": "Port" },
-            { "title": "Visible" },
-            ],
-            "data": [],
-            "ready": false
-        };
+    var usersTable ={
+    				"data": [],
+    				"ready": false
+    				}
 
+    var usersTableColumns = [{label:"userId", map: "userId" },
+                             {label:"username", map: "username" },
+                             {label:"isDeleted", map: "isDeleted" }
+							];
 
     usersService.query().$promise.then(
             function (users) {
             	console.log("users:=>",users);
                 users.forEach(function (user) {
-                    var userData = []
-                    userData.push({ "value": user.userId });
-                    userData.push({ "value": user.username });
-                    userData.push({ "value": user.isDeleted });
+                    var userData = [];
+                    userData.push( {"value": user.userId} );
+                    userData.push( {"value": user.username} );
+                    userData.push( {"value": user.isDeleted});
                     usersTable.data.push(userData);
-                    
+                   
+
                 });
             })    
             .then(function () {
                 $scope.usersTable = usersTable;
                 $scope.usersTable.ready = true;
+                console.log("uuuu", usersTable.data)
             });
 
 
-
+    ;
 }]);
 
