b76017f2407d33f49ae764d132060cb95dde5366
d.charlaftis <d.charlaftis@dotbydot.gr>
2015-09-28 15:09:33 +0300

    rt

--- app/layout/tableDirective.js
+++ app/layout/tableDirective.js
@@ -20,74 +20,25 @@ app.directive('myTable', ['$timeout', '$log', '$location', 'scopeComService', fu
                             $scope.toolbar_width = "col-md-12";
 
                         var table = $('#' + $scope.tableid);
-                        $.extend(true, $.fn.DataTable.TableTools.classes, {
-                            "container": "btn-group tabletools-dropdown-on-portlet",
-                            "buttons": {
-                                "normal": "btn btn-sm default",
-                                "disabled": "btn btn-sm default disabled"
-                            },
-                            "collection": {
-                                "container": "DTTT_dropdown dropdown-menu tabletools-dropdown-menu"
-                            }
-                        });
-
-                        var init = {
-                            "language": {
-                                "aria": {
-                                    "sortAscending": ": activate to sort column ascending",
-                                    "sortDescending": ": activate to sort column descending"
-                                },
-                                "emptyTable": "No data available in table!",
-                                "info": "Showing _START_ to _END_ of _TOTAL_ entries",
-                                "infoEmpty": "No entries found",
-                                "infoFiltered": "(filtered from _MAX_ total entries)",
-                                "lengthMenu": "Show _MENU_ entries",
-                                "search": "Search:",
-                                "zeroRecords": "No matching records found"
-                            },
-                            "order": [
-                                [0, 'asc']
-                            ],
-                            "lengthMenu": [
-                                [5, 15, 20, -1],
-                                [5, 15, 20, "All"]
-                            ],
-                            "pageLength": 20,
-
-                            "dom": "<'row' <'col-md-12'T>><'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r><'table-scrollable't><'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>", // horizobtal scrollable datatable
-                           
-                            
-                        }
-
-
-                        var oTable = table.dataTable(init);
-                                              
-                        //////////////////////////////////////////////////////////////
-/*
+                  
+                         var oTable = table.dataTable();                     
+                      
                         table.on('click', '.edit', function (e2) {
                             e2.preventDefault();
 
                             var nRow = $(this).parents('tr')[0];
                             var editline = oTable.fnGetPosition(nRow);
-
-                            $scope.idT = $scope.tabledata.data[editline][0].value;
-                            $scope.domainT = $scope.tabledata.data[editline][1].value;
-                            $scope.portT = $scope.tabledata.data[editline][2].value;
-                            $scope.visibleT = $scope.tabledata.data[editline][3].value;
+                                    //$scope.tabledata.data[editline][0]
+                             alert( $scope.tabledata.data[editline][1].value );
 
                             //write data to registered service scopeCommService     
-                            scopeComService.add($scope.idT);
-                            scopeComService.add($scope.domainT);
-                            scopeComService.add($scope.portT);
-                            scopeComService.add($scope.visibleT);
+                           // scopeComService.add($scope.idT);
                            
-                            //console.log($scope.domainT); console.log($scope.portT); console.log($scope.visibleT);
-
-                            $location.path('/portal/edit');
-                            $scope.$apply();
+                           //$location.path('/portal/edit');
+                           // $scope.$apply();
                         });
                                               
-*/
+
                       
                     }, 0);
                 }
