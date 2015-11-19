d5935d449b4adc6c0559141c4aaa201272bd5962
Giannis Vagias <g.vagias@dotbydot.gr>
2015-11-19 13:12:01 +0200

    more cleanup config

--- PRPServerCAS/dontsync_example.js
+++ PRPServerCAS/dontsync_example.js
@@ -14,10 +14,9 @@ config.development = {
   },
  
   //web server settigns
-  serverPort: 4457 || process.env.port || 1337,
+  serverPort: 4457 || process.env.port,
   useWebHttps: false,
   serverHost: "echidna.dotbydot.eu",
-  server: "https://echidna.dotbydot.eu:4457/",
 
   //cors settigns
   validDomains: "*",
@@ -35,16 +34,10 @@ config.development = {
   other: {}
 };
 
-// config when running from the host of vagrant (NODE_ENV=host)
-config.host = ld.cloneDeep(config.development);
-config.host.env = 'host';
-config.host.serverPort = process.env.PORT || 1337;
 
 // config when running on a production server (NODE_ENV=production)
 config.production = ld.cloneDeep(config.development);
 config.production.env = 'production';
-config.production.pmxActive = true;
-config.production.SQLlogging = false;
 config.production.loglevel = {error:true, warn:true, log:false,  info:false};
 
 module.exports = config;
