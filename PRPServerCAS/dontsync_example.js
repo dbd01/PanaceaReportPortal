7f80b95dbd44b17273bf31db7cbca0a2ae2ffb2c
Giannis Vagias <g.vagias@dotbydot.gr>
2015-11-19 15:54:41 +0200

    ..

--- PRPServerCAS/dontsync_example.js
+++ PRPServerCAS/dontsync_example.js
@@ -1,23 +1,24 @@
+
 "use strict"
 var config = {};
 
 // default config for running within vagrant
 // exceptions for running in different evironments are set at the end
 //web server settigns
-config.serverPort: 4457 ,
-config.useWebHttps: false,
-config.serverHost: "echidna.dotbydot.eu",
+config.serverPort= 4457;
+config.useWebHttps= false;
+config.serverHost= "echidna.dotbydot.eu";
 
 //cors settigns
-config.validDomains: "*",
+config.validDomains= "*";
 
 //polyphemus settings
-config.applicationName: "PRPServerCAS",
-config.polyphemusPort: 4451,
-config.polyphemusHost: "echidna.dotbydot.eu",
-config.polyphemusActive: true,
-config.polyphemusHttps: true,
-config.useCAS:true,
-config.CASService:"http://echidna.dotbydot.eu:4457/cas"
+config.applicationName= "PRPServerCAS";
+config.polyphemusPort= 4451;
+config.polyphemusHost= "echidna.dotbydot.eu";
+config.polyphemusActive= true;
+config.polyphemusHttps= true;
+config.useCAS=true;
+config.CASService="http://echidna.dotbydot.eu:4457/cas";
 
 module.exports = config;
