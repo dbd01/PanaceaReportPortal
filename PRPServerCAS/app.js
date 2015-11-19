52a19c4633c3ace79f2cf6eb858ff6b33382c594
Giannis Vagias <g.vagias@dotbydot.gr>
2015-11-19 12:46:48 +0200

    fix polyphemus-connector typo

--- PRPServerCAS/app.js
+++ PRPServerCAS/app.js
@@ -20,7 +20,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser'); 
+var bodyParser = require('body-parser');
 
 var app = express();
 
