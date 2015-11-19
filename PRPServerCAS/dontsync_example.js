78c95096bb0d447a6f6e57b86b697a82e6ab025e
Giannis Vagias <g.vagias@dotbydot.gr>
2015-11-19 12:11:14 +0200

    test port

--- PRPServerCAS/dontsync_example.js
+++ PRPServerCAS/dontsync_example.js
@@ -14,7 +14,7 @@ config.development = {
   },
  
   //web server settigns
-  serverPort: 4457 || process.env.port || 1337,
+  serverPort: 4457 ,
   useWebHttps: false,
   serverHost: "echidna.dotbydot.eu",
   server: "https://echidna.dotbydot.eu:4457/",
