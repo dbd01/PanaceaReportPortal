d76f5e81a077e0f19bfcf3816d3d91c82bc2497d
Giannis Vagias <g.vagias@dotbydot.gr>
2015-11-20 18:18:13 +0200

    test redirection to panaceaReportPortal client

--- PRPServerCAS/prpservercas.js
+++ PRPServerCAS/prpservercas.js
@@ -6,4 +6,5 @@ app.set('port', config.serverPort);
 
 var server = app.listen(app.get('port'), function () {
   console.time().file().log('Express server listening on port ' + server.address().port);
+
 });
