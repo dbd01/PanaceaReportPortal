386c2e510a92e3e0fe6e3881f0a00db376094caf
d.charlaftis <d.charlaftis@dotbydot.gr>
2015-11-23 13:22:37 +0200

    fgh

--- app/auth/controllers/authenticationController.js
+++ app/auth/controllers/authenticationController.js
@@ -1,33 +1,26 @@
 'use strict';
 
-    app.controller('authenticationController', ['$scope', 'localStorageService', '$location', 'navigationService', '$stateParams', 
-    	function ($scope, localStorageService, $location, navigationService, $stateParams) {
+    app.controller('authenticationController', ['$scope', 'localStorageService', '$location', 'navigationService', '$stateParams', '$rootScope', 
+    	function ($scope, localStorageService, $location, navigationService, $stateParams, $rootScope) {
+	    	console.log("dddd", $stateParams);
+	        console.log("GGG", $location.path());     
+	                              	       		
+	        //set the token  		               
+	        localStorageService.set('authorizationData', 
+	         	{ token: $stateParams.token,              	   
+	         	  expires: $stateParams.expires		                 	   
+	         	});
+	        
+	        $rootScope.log_link.value = "Logout";
 
-    	console.log("dddd", $stateParams);
-        console.log("GGG", $location.path());   
-		 
-			     
-                               
-		       		
-/*
-            //set the token  		               
-            localStorageService.set('authorizationData', 
-             	{ token: response.token, 
-             	  userName: $scope.authData.userName, 
-             	  expires: response.expires		                 	   
-             	});
-            
-            $rootScope.log_link.value = "Logout";
+	        //get the last desired navigation
+	        var navLocation = navigationService.list[navigationService.list.length-1];
+	        navigationService.flush();
 
-            //get the last desired navigation
-            var navLocation = navigationService.list[navigationService.list.length-1];
-            navigationService.flush();
+	        if (navLocation==null)
+	        	$location.path('/welcome');
+	        else
+	        	$location.path('/'+navLocation);    	 
 
-            consoleService.printIt("navLocation=..", navLocation);
-            if (navLocation==null)
-            	$location.path('/welcome');
-            else
-            	$location.path('/'+navLocation);    	 
-*/
 	}]);
 		
