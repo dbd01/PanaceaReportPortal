aa779eaa6d1ba2f4bd55634f23548328b48c3dbc
d.charlaftis <d.charlaftis@dotbydot.gr>
2015-11-23 18:27:36 +0200

    vbn

--- app/users/userInfoDirective.js
+++ app/users/userInfoDirective.js
@@ -37,16 +37,19 @@ app.directive('userinfo', [ 'localStorageService', 'usersService' ,'consoleServi
 
                          if( $scope.showIt){
                             console.log("zzzz",$scope.userData );
-                            
+
                             $scope._id= $scope.userData[0].value;
                             $scope.username = $scope.userData[1].value;
                             $scope.isDeleted= $scope.userData[2].value;
-                            $scope.password = $scope.userData[3].value;  
+                            $scope.email= $scope.userData[3].value;
+                            $scope.confirmed= $scope.userData[4].value;
+                            $scope.active= $scope.userData[5].value;
+                            $scope.password = $scope.userData[6].value;  
                             //scope.groups must contain strings
-                            for (var i=0; i<$scope.userData[4].length; i++)                           
+                            for (var i=0; i<$scope.userData[7].length; i++)                           
                                  $scope.groupz[i] = {
-                                                        "id": $scope.userData[4][i]._id,
-                                                        "name": $scope.userData[4][i].name
+                                                        "id": $scope.userData[7][i]._id,
+                                                        "name": $scope.userData[7][i].name
                                                     }                                                                                  
                          }
                          else 
