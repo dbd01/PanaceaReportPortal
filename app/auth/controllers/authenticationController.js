781d4b25dd34df25973529c6f079fdb6bb2600dd
d.charlaftis <d.charlaftis@dotbydot.gr>
2015-11-23 14:10:32 +0200

    erer

--- app/auth/controllers/authenticationController.js
+++ app/auth/controllers/authenticationController.js
@@ -1,21 +1,38 @@
 'use strict';
 
-    app.controller('authenticationController', ['$scope', 'localStorageService', '$location', 'navigationService', '$stateParams', '$rootScope', 
-    	function ($scope, localStorageService, $location, navigationService, $stateParams, $rootScope) {
+    app.controller('authenticationController', ['$scope', 'localStorageService', '$location', 'navigationService', '$stateParams', '$rootScope','$http','appSettings', 
+    	function ($scope, localStorageService, $location, navigationService, $stateParams, $rootScope, $http, appSettings) {
+	    	$rootScope.urlLink = {value:"true"};
+	    	
 	    	console.log("dddd", $stateParams);
-	        console.log("GGG", $location.path());     
-	                              	       		
+	        console.log("tt", $location.absUrl());  
+	         
 	        //set the token  		               
 	        localStorageService.set('authorizationData', 
 	         	{ token: $stateParams.token,              	   
 	         	  expires: $stateParams.expires		                 	   
 	         	});
+
+            //make a test call to see if the token provided in the url is correct
+             $http.get(appSettings.authServerPath + '/api/v1/user/').
+                    success(function (response, status) {
+                       console.log('sucessss->', response);
+                    }).
+                    error(function (response, status) {
+                        
+                        console.log('errorrrr44->', response);
+                    });
+             
+
 	        
-	        $rootScope.log_link.value = "Logout";
+	        if (localStorageService.get('authorizationData')==null)
+	        	$rootScope.log_link.value = "Login";
+	        else
+	        	$rootScope.log_link.value = "Logout";
 
 	        //get the last desired navigation
 	        var navLocation = navigationService.list[navigationService.list.length-1];
-	        navigationService.flush();
+	        navigationService.flush();            
 
 	        if (navLocation==null)
 	        	$location.path('/welcome');
--- app/common/settingsModule.js
+++ app/common/settingsModule.js
@@ -4,7 +4,7 @@
         .module("settingsModule",[])
         .constant("appSettings",
         {
-            authServerPath: "http://192.168.11.239:10001",
+            authServerPath: "https://echidna.dotbydot.eu:4451",
             localPath: "http://localhost:8080"
         });
 })(angular);
