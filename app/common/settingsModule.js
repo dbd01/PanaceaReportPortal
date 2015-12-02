da00823c4cf1f5ce200ad98fa836567f49cff6e5
Giannis Vagias <g.vagias@dotbydot.gr>
2015-12-02 15:53:04 +0200

    ..

--- app/common/settingsModule.js
+++ app/common/settingsModule.js
@@ -3,14 +3,10 @@
   ng
   .module("settingsModule",[])
   .constant("appSettings",{
-    authServerPath: "http://localhost:4451",
-    casPath : "http://localhost:4457/cas/login/",
-    casLogoutPath : "http://localhost:4457/cas/logout", 
+    authServerPath: "https://echidna.dotbydot.eu:4451",
+    casPath : "https://echidna.dotbydot.eu:4457/cas/login/",
+    casLogoutPath : "https://echidna.dotbydot.eu:4457/cas/logout", 
     localPath: "http://localhost:8080"
   });
 })(angular);
 
-/*authServerPath: "https://echidna.dotbydot.eu:4451",
-    casPath : "https://echidna.dotbydot.eu:4457/cas/login/",
-    casLogoutPath : "https://echidna.dotbydot.eu:4457/cas/logout", 
-    localPath: "http://localhost:8080"*/
