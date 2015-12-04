a190f54da7f109bf206987766358e06367cfede8
gvagias <g.vagias@dotbydot.gr>
2015-12-04 14:46:07 +0200

    display groups permissions on application view mode

--- app/applications/applicationInfoDirective.js
+++ app/applications/applicationInfoDirective.js
@@ -18,39 +18,25 @@ app.directive('applicationinfo', [ 'localStorageService','consoleService' , 'app
               $scope.groups =[];
               for (var i=0; i<$scope.tabledata.data.length; i++){
                 $scope.groups[i] = {
-                  "id": $scope.tabledata.data[i][0].value,
-                  "name": $scope.tabledata.data[i][1].value
+                  "permissions": $scope.tabledata.data[i].permissions,
+                  "id": $scope.tabledata.data[i]._id,
+                  "name": $scope.tabledata.data[i].name
                 }
               }
               consoleService.printIt("eeeeeeggggg0000ee",  $scope.groups );
               //get the data from the service
               $scope.mode = scopeComService.list[0];
               console.log("mode: ", $scope.mode);
-              if ($scope.mode=="view"){
+              if ($scope.mode=="view")
                 $scope.applicationData= scopeComService.list[1];
-                $scope.view=true;
-                $scope.edit = false;
-                $scope.add=false;
-              }
-              else if ($scope.mode=="edit"){
+              else if ($scope.mode=="edit")
                 $scope.applicationData= scopeComService.list[1];
-                $scope.view=false;
-                $scope.edit = true;
-                $scope.add=false;
-              }
-              else if ($scope.mode=="add"){
-                $scope.view=false;
-                $scope.edit = false;
-                $scope.add=true;
-              }
-              console.log("view: ", $scope.view);
-              console.log("edit: ", $scope.edit);
-              console.log("add: ", $scope.add);
+
               scopeComService.flush();
               $scope.groupz = [];
               $scope.groupzIDz =[]; 
               
-              if($scope.view){
+              if($scope.mode=="view" || $scope.mode=="edit"){
                 console.log("view: ", $scope.applicationData[4])
                 $scope._id= $scope.applicationData[0].value;
                 $scope.name = $scope.applicationData[1].value;
@@ -61,20 +47,21 @@ app.directive('applicationinfo', [ 'localStorageService','consoleService' , 'app
                   $scope.groupz[i] = {
                     "id": $scope.applicationData[4][i]._id,
                     "name": $scope.applicationData[4][i].name,
-                    "description": $scope.applicationData[4][i].description,
                     "permissions": []
                   }
                   for (var j=0; j<$scope.applicationData[4][i].permissions.length; j++){
-                    $scope.groupz[i].permissionz[j] = {
-                      "id": $scope.applicationData[4][i]._id,
-                      "name": $scope.applicationData[4][i].name,
-                      "description": $scope.applicationData[4][i].description
+                    $scope.groupz[i].permissions[j] = {
+                      "_id": $scope.applicationData[4][i].permissions[j]._id,
+                      "name": $scope.applicationData[4][i].permissions[j].name,
+                      "description": $scope.applicationData[4][i].permissions[j].description,
+                      "type": $scope.applicationData[4][i].permissions[j].type,
+                      "model": $scope.applicationData[4][i].permissions[j].model,
+                      "url": $scope.applicationData[4][i].permissions[j].url
                     }
                   }
                 }
               }
-              else if($scope.edit){
-                console.log("edit: ", $scope.applicationData[4])
+              /*else if($scope.edit || $scope.view){
                 $scope._id= $scope.applicationData[0].value;
                 $scope.name = $scope.applicationData[1].value;
                 $scope.description= $scope.applicationData[2].value;
@@ -86,7 +73,7 @@ app.directive('applicationinfo', [ 'localStorageService','consoleService' , 'app
                     "name": $scope.applicationData[4][i].name
                   }
                 }
-              }
+              }*/
               else
                 $scope.name = "";
 
@@ -97,7 +84,6 @@ app.directive('applicationinfo', [ 'localStorageService','consoleService' , 'app
                 "url": $scope.url,
                 "groups": $scope.groupz
               }
-              console.log("mode: ",$scope.mode, $scope.applicationData)
               $scope.closeAlert = function() {
                 $scope.alert=null;
                 $scope.applicationData={
@@ -174,6 +160,11 @@ app.directive('applicationinfo', [ 'localStorageService','consoleService' , 'app
                   }
                 });
               }
+              $scope.edit = function(){
+                $scope.mode="edit";
+              }
+              console.log("applicationData data: ", $scope.applicationData)
+              console.log("groups: ", $scope.groups)
             },0);
           }
         });
