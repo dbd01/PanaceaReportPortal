add86c5f429a3301963e5896fc413a8574651efa
gvagias <g.vagias@dotbydot.gr>
2015-12-07 22:04:13 +0200

    ..

--- app/common/settingsModule.js
+++ app/common/settingsModule.js
@@ -3,16 +3,10 @@
   ng
   .module("settingsModule",[])
   .constant("appSettings",{
-    authServerPath: "http://localhost:4451",
-    casPath : "http://localhost:4457/cas/login/",
-    casLogoutPath : "http://localhost:4457/cas/logout", 
-    localPath: "http://localhost:4453"
-  });
-})(angular);
-
-/*
- authServerPath: "https://echidna.dotbydot.eu:4451",
+    authServerPath: "https://echidna.dotbydot.eu:4451",
     casPath : "https://echidna.dotbydot.eu:4457/cas/login/",
     casLogoutPath : "https://echidna.dotbydot.eu:4457/cas/logout", 
     localPath: "http://localhost:8080"
-*/
+  });
+})(angular);
+
