0c40672f1b11ee59c9773825f34f2f877c015050
Giannis Vagias <g.vagias@dotbydot.gr>
2016-01-14 16:53:05 +0200

    multilingual messages

--- ext-modules/dbdGridView/dbdGridViewDirective.js
+++ ext-modules/dbdGridView/dbdGridViewDirective.js
@@ -2,8 +2,8 @@
   "use strict";
   
   angular.module("dbdGridViewModule").directive("dbdGridView", dbdGridView);
-  dbdGridView.$inject= ['$state', '$timeout', 'scopeComService', '$rootScope', '$location', 'DTOptionsBuilder'];
-  function dbdGridView($state, $timeout, scopeComService, $rootScope, $location, DTOptionsBuilder) {
+  dbdGridView.$inject= ['$state', '$timeout', 'scopeComService', '$rootScope', '$location', 'exceptionService'];
+  function dbdGridView($state, $timeout, scopeComService, $rootScope, $location, exceptionService) {
     return {
       controller: "dbdGridViewController",
       templateUrl: "ext-modules/dbdGridView/dbdGridViewTemplate.html",
@@ -43,9 +43,29 @@
               else
                 $scope.toolbar_width = "col-md-12";
 
+              var mode=$scope.tabledata.mode;
+              var customMessages={
+                addNewEntityError:{
+                  en:"Non valid mode: "+mode+" for add new entity.",
+                  el:"Μη έγκυρη κατάσταση φόρμας: "+mode+" για προσθήκη νέας οντότητας."
+                },
+                editEntityError:{
+                  en:"Non valid mode: "+mode+" for edit entity.",
+                  el:"Μη έγκυρη κατάσταση φόρμας: "+mode+" για επεξεργασία οντότητας."
+                },
+                deleteEntityMessage:{
+                  en:function(entityName, entity){
+                    return "Are you sure you want to delete " + entity + " " + entityName +" ?"
+                  },
+                  el:function(entityName, entity){
+                    return "Σίγουρα θέλετε να διαγράψετε την οντότητα " + entity + " " + entityName +" ;"
+                  }
+                }
+              }
+              
               $scope.addNewEntity = function(){
-                if ($scope.tabledata.mode=="deleted"){
-                  //error
+                if (mode=="deleted"){
+                  exceptionService.catcher(customMessages.addNewEntityError[$rootScope.lang])(error);
                 }
                 else{
                   $state.go($scope.tabledata.detailView+'.new'+$scope.tabledata.entityC);
@@ -53,30 +73,22 @@
               }
               $scope.view_entity= function(editline){
                 var id=$scope.tabledata.data[editline][0].value;
-                //scopeComService.add(id);
-                if ($scope.tabledata.mode=="deleted"){
-                  //$state.go($scope.tabledata.detailView+'.deleted'+$scope.tabledata.entityC);
+                if (mode=="deleted"){
                   var path=$scope.tabledata.detailView+'/deleted/'+id;
-                  console.log("path:", path);
                   $location.path(path);
                 }
                 else{
-                  //$state.go($scope.tabledata.detailView+'.view'+$scope.tabledata.entityC);
                   var path=$scope.tabledata.detailView+'/view/'+id;
-                  console.log("path:", path);
                   $location.path(path);
                 }
               }
               $scope.edit_entity= function(editline){
                 var id=$scope.tabledata.data[editline][0].value;
-                //scopeComService.add(id);
-                if ($scope.tabledata.mode=="deleted"){
-                  //error
+                if (mode=="deleted"){
+                  exceptionService.catcher(customMessages.editEntityError[$rootScope.lang])(error);
                 }
                 else{
-                  //$state.go($scope.tabledata.detailView+'.edit'+$scope.tabledata.entityC);
                   var path=$scope.tabledata.detailView+'/edit/'+id;
-                  console.log("path:", path);
                   $location.path(path);
                 }
               }
@@ -87,20 +99,15 @@
                 var entityName, entity="";
                 entityName = $scope.tabledata.data[editline][1].value;
                 entity=$scope.tabledata.entity;
-                if (confirm("Are you sure you want to delete " + entity + " " + entityName +" ?")){
+                if (confirm(customMessages.deleteEntityMessage[$rootScope.lang](entityName, entity))){
                   var id=$scope.tabledata.data[editline][0].value;
-                  //scopeComService.add(id);
-                  //$state.go($scope.tabledata.detailView+'.remove'+$scope.tabledata.entityC);
                   var path=$scope.tabledata.detailView+'/remove/'+id;
-                  console.log("path:", path);
                   $location.path(path);
                 }
                 /*bootbox.confirm("Are you sure you want to delete " + entity + " <b>" + entityName +"</b> ?", function(ok){
                   console.log(ok)
                   if (ok){
                     var id=$scope.tabledata.data[editline][0].value;
-                    //scopeComService.add(id);
-                    //$state.go($scope.tabledata.detailView+'.remove'+$scope.tabledata.entityC);
                     var path=$scope.tabledata.detailView+'/remove/'+id;
                     console.log("path:", path);
                     $location.path(path);
@@ -119,9 +126,6 @@
               $scope.tabledata.headers[i].title=$scope.tabledata.header[i].title[$scope.lang];
             };
           }
-          
-          //$scope.ready="false";
-          //$scope.ready="true";
         });
       }
     }
