db343fca64379bffd1767adead2666885e299181
Giannis Vagias <g.vagias@dotbydot.gr>
2016-01-15 13:22:48 +0200

    ..

--- ext-modules/polyphemus/polyphemusController.js
+++ ext-modules/polyphemus/polyphemusController.js
@@ -15,7 +15,6 @@
       $scope.authData.username="";
       $scope.authData.password = "";
       $scope.alert=null;
-      //$location.path('/login');
     }
     $scope.login = function () {
       console.log("polyphemusController: login: authData: ", $scope.authData);
@@ -30,21 +29,17 @@
             userName: $scope.authData.username, 
             expires: response.expires
           };
-          //$rootScope.authorizationData=authorizationData;
-          //$rootScope.$broadcast("authData");
           console.log("polyphemusController: login: $broadcast");
-          //$rootscope.broadcast(authorizationData);
           polyphemusCommServiceOut.setValue(authorizationData);
         },
         //error
-        function (response) {
-          console.log("data..>",$scope.authData);
-          if (response.data == null){
+        function (error) {
+          if (error.data == null){
             console.log("response data is null!");
             $scope.message = "Server error";
           }
           else{
-            console.log("response error: ", response);
+            console.log("response error: ", error);
             $scope.alert = { 
               type: 'danger', 
               msg: 'Wrong Username or password' 
