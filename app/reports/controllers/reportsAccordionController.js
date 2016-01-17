41fb84a4fa86f473ee9f7fcda0edf9b5ab98725f
gvagias <g.vagias@dotbydot.gr>
2016-01-17 14:57:59 +0200

    acco semi-working

--- app/reports/controllers/reportsAccordionController.js
+++ app/reports/controllers/reportsAccordionController.js
@@ -2,21 +2,43 @@
 "use strict";
 
   angular.module('PanaceaReports').controller("reportsAccordionController", reportsAccordionController);
-  reportsAccordionController.$inject= ['$scope', 'reportsAccordionService'];
-  function reportsAccordionController($scope, reportsAccordionService) {
+  reportsAccordionController.$inject= ['$scope', '$element' ];
+  function reportsAccordionController($scope, $element) {
     var accordionObj={
       "data":[],
       "ready":false
     };
-    reportsAccordionService.then(
-      function (data){
-        accordionObj.data=data;
-        accordionObj.ready=true;
-        $scope.accordionObj = accordionObj;
-        console.log("AccordionController: AccordionService: accordionObj: ", $scope.accordionObj)
-      },
-      function (error){
-        console.log("error: ", error)
-      });
+
+    var accoTitles=$element.find(".accotitle");
+    var accoContents=$element.find(".accocontent");
+
+    console.log(accoTitles);
+    console.log(accoContents);
+
+    if (accoTitles.length!=accoContents.length){
+      //error
+    }
+    else{
+      for (var i = 0; i < accoTitles.length; i++) {
+        var accoTitleStr=accoTitles[i].innerHTML;
+        accoTitles[i].innerHTML="";
+        var accoContentStr=accoContents[i].innerHTML;
+        accoContents[i].innerHTML="";
+
+        console.log(accoTitleStr);
+        console.log(accoContentStr);
+
+        var accoItem={
+          title: accoTitleStr,
+          data: accoContentStr
+        }
+        
+        accordionObj.data.push(accoItem);
+      };
+
+      accordionObj.ready=true;
+      $scope.accordionObj = accordionObj;
+      console.log("AccordionController: AccordionService: accordionObj: ", $scope.accordionObj)
+    }
   };
 })();
