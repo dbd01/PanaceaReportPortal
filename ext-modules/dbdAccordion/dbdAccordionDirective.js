198d291c2d0cb1b969d9d9a9de924a6435ae124f
gvagias <g.vagias@dotbydot.gr>
2016-01-17 20:26:42 +0200

    more junk removed

--- ext-modules/dbdAccordion/dbdAccordionDirective.js
+++ ext-modules/dbdAccordion/dbdAccordionDirective.js
@@ -2,16 +2,15 @@
   "use strict";
   
   angular.module("dbdAccordionModule").directive("dbdAccordion", dbdAccordion);
-  dbdAccordion.$inject= ['$timeout', '$rootScope', '$sce'];
-  function dbdAccordion($timeout, $rootScope, $sce) {
+  dbdAccordion.$inject= ['$timeout'];
+  function dbdAccordion($timeout) {
     return {
       transclude: true,
       restrict: 'E',
-      templateUrl:"ext-modules/dbdAccordion/dbdAccordionTemplate.html",
+      template:"<div ng-transclude></div>",
       scope: {
         accohtml:'=',
-        ready: '@',
-        lang: '='
+        ready: '@'
       },
       link: function ($scope, element, attrs) {
         $scope.$watch('ready', function (newvalue, oldvalue) {
@@ -25,10 +24,6 @@
             $scope.accohtml.accordion();
            });
          }
-
-        $rootScope.$watch('lang', function(newvalue, oldvalue){
-          $scope.lang=$rootScope.lang;
-        });
       }
     }
   };
