15c2529c001475579cdac0bc2e5a9e6432a75f39
gvagias <g.vagias@dotbydot.gr>
2016-04-04 13:41:04 +0300

    test pandora

--- app/common/settingsModule.js
+++ app/common/settingsModule.js
@@ -4,8 +4,8 @@
   .module("settingsModule",[])
   .constant("appSettings",{
     authServerPath: "https://pandora.dotbydot.eu:4451",
-    casPath : "http://localhost:4457/cas/login/",
-    casLogoutPath : "http://localhost:4457/cas/logout", 
+    casPath : "https://pandora.dotbydot.eu:4457/cas/login/",
+    casLogoutPath : "https://pandora.dotbydot.eu:4457/cas/logout", 
     localPath: "http://localhost:8080"
   });
 })(angular);
